f9719b6c00f310e0895ff64ca9f479aaa30503e8
vova <vovanychkov95@gmail.com>
2019-06-30 17:15:12 +0300

    hide keyboard

--- app/src/main/java/com/kovrizhkin/moviesapp/view/MainActivity.kt
+++ app/src/main/java/com/kovrizhkin/moviesapp/view/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.kovrizhkin.moviesapp.view
 
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
+import android.view.KeyEvent
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.widget.TextView
 import com.kovrizhkin.moviesapp.MainContract
 import com.kovrizhkin.moviesapp.R
 import com.kovrizhkin.moviesapp.model.pojo.Movie
@@ -32,7 +37,21 @@ class MainActivity : AppCompatActivity(), MainContract.View {
         presenter = MainPresenter(this)
 
         presenter.loadMovies()
-        //presenter.searchMovies("История")
+
+        search_edit_text.setOnEditorActionListener { v, actionId, _ ->
+
+            val searchQuery = v.text.toString()
+            if (actionId == EditorInfo.IME_ACTION_SEARCH && searchQuery.isNotEmpty()) {
+
+                presenter.searchMovies(searchQuery)
+
+                val imm = v.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.hideSoftInputFromWindow(v.windowToken, 0)
+                return@setOnEditorActionListener true
+            }
+            return@setOnEditorActionListener false
+        }
+
     }
 
     override fun showResult(result: List<Movie>) {
